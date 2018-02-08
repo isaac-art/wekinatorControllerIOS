0fd4ef248ca7bf5656e32dc3effcc8ba8cfc4e5e
Isaac Clarke <isaac@ishimura.uk>
2018-02-08 15:45:26 +0000

    Initial Commit

---
+++ OSCtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSCtest
+//
+//  Created by Isaac Clarke on 08/02/2018.
+//  Copyright © 2018 Isaac Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
